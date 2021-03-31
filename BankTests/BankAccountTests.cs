13bdbc8a06ea87d5e59f246dc2e17176d0abebf4
Steffanie Graner <graner.steffanie@gmail.com>
2021-03-31 16:42:14 -0300

    Build and run the test

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
 
 namespace BankTests
 {
@@ -6,8 +7,20 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_WithValidAmount_UpdatesBalance()
         {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Joao Silva", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
         }
     }
 }
